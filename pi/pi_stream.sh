b4e041b2e629862efca14d94fda640fef3b04ff3
Linsheng Pan <141114823+ray-uncoding@users.noreply.github.com>
2025-06-19 15:28:14 +0800

    變更路徑

--- pi/pi_stream.sh
+++ pi/pi_stream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 切換到專案資料夾
-cd /home/pi2/pi_cam_stream/pi
+cd /home/pi_cam_stream/pi
 
 # 啟動 pi_stream.py
 echo "正在啟動 pi_stream.py..."
--- pi/setup_pi.sh
+++ pi/setup_pi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 切換到專案資料夾
-cd /home/pi2/pi_cam_stream/pi
+cd /home/pi_cam_stream/pi
 
 # 檢查必要文件
 if [ ! -f "pi_requirements.txt" ]; then
@@ -13,6 +13,10 @@ fi
 echo "正在檢查並安裝必要的圖形庫..."
 sudo apt update
 sudo apt install python3-pil python3-pil.imagetk -y
+
+# 安裝 OpenCV
+echo "正在安裝 OpenCV..."
+sudo apt install python3-opencv -y
 
 # 安裝 Python 套件
 echo "正在安裝 Python 套件..."
